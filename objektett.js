8fc6f6e02d210847a45223327ee2eea2a7ffeff0
Alekssyla <alekssyla2@gmail.com>
2024-01-10 16:03:28 +0100

    firts commit

---
+++ objektett.js
@@ -0,0 +1,14 @@
+let fruits ={
+    molle:"jeshile",
+    rrush:"i kuq",
+    banane:"e verdhe",
+
+};
+ let keys=Object.keys(fruits);
+ for(let i=0; i< keys.length; i++){
+
+    let key = keys[i];
+
+    console.log(key,fruits[key])
+ }
+ 
